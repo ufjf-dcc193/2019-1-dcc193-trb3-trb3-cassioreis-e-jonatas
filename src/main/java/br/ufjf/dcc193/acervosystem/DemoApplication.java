db6f1b9528de9a8328ad517d48c92ef0c300cddd
Jônatas <jjsfandre@gmail.com>
2019-07-03 17:25:24 -0300

    Retirando códigos obsoletos

--- src/main/java/br/ufjf/dcc193/acervosystem/DemoApplication.java
+++ src/main/java/br/ufjf/dcc193/acervosystem/DemoApplication.java
@@ -49,32 +49,9 @@ public class DemoApplication {
 		Etiqueta e2 = new Etiqueta("Etiqueta 2", "Descrição etiqueta 2", "url2.com.br");
 		Etiqueta e3 = new Etiqueta("Etiqueta 3", "Descrição etiqueta 3", "url3.com.br");
 		
-		Anotacao a= new Anotacao("Anotação 1", "Descrição anotação 1", "url-anot.com.br", "01-07-19");
-		Anotacao a2= new Anotacao("Anotação 2", "Descrição anotação 2", "url-anot2.com.br", "02-07-19");
-		Anotacao a3= new Anotacao("Anotação 3", "Descrição anotação 3", "url-anot3.com.br", "03-07-19");
-		a.setUsuario(u);
-		a2.setUsuario(u);
-		
-		Vinculo v = new Vinculo(true);
-		v.setItemOrigem(i);
-		v.setItemDestino(i2);
-		v.addEtiqueta(e3);
-		v.addAnotacao(a3);
-		
-		/*i.addAnotacao(a);
-		i.addEtiqueta(e);
-		i.addEtiqueta(e2);
-		
-		i2.addAnotacao(a2);
-		i2.addEtiqueta(e);
-		i2.addEtiqueta(e2); */
-		
 		
 		usuarioRepository.save(u);		
 		usuarioRepository.save(u2);		
-		anotacaoRepository.save(a);
-		anotacaoRepository.save(a2);
-		anotacaoRepository.save(a3);
 		etiquetaRepository.save(e);
 		etiquetaRepository.save(e2);
 		etiquetaRepository.save(e3);
--- src/main/java/br/ufjf/dcc193/acervosystem/controller/HomeController.java
+++ src/main/java/br/ufjf/dcc193/acervosystem/controller/HomeController.java
@@ -40,7 +40,7 @@ public class HomeController {
         else
             mv.setViewName("index-nologged");
 
-        mv.addObject("objetos",listarTodosOsObjetos());
+        //mv.addObject("objetos",listarTodosOsObjetos());
         return mv;
     }
 
